f3ff5513c91ae26c91ef906f332d0b72b0e6a1d7
trainthunder <kitikun.po@gmail.com>
2024-07-03 16:32:38 +0700

    feat: Hackhour Algorithm

--- ex-3.js
+++ ex-3.js
@@ -1,4 +1,13 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function multiplicationTable() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function multiplicationTable(n) {
+  let finalResult = "";
+  for (let i = 1; i <= n; i++) {
+    let rowResult = "";
+    for (let j = 1; j <= n; j++) {
+      rowResult += j * i + "\t";
+    }
+    finalResult += rowResult + "\n";
+  }
+  return finalResult;
 }
+console.log(multiplicationTable(3));
